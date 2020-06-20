e5ef9bd1cf55f781945abb6448f8bfc59c87764d
cordovat <cordovat@gmail.com>
2020-06-20 04:03:07 +0200

    Cambio de sun.misc.BASE64Decoder() a java.util.Base64.getMimeDecoder() para compatibilidad con JDK 11

--- src/com/apireportes/seg/DesEncrypter.java
+++ src/com/apireportes/seg/DesEncrypter.java
@@ -28,7 +28,8 @@ import java.io.*;
                 byte[] enc = ecipher.doFinal(utf8);
 
                 // Encode bytes to base64 to get a string
-                return new sun.misc.BASE64Encoder().encode(enc);
+                byte[] bytes = java.util.Base64.getMimeEncoder().encode(enc);
+                return new String(bytes);
             } catch (javax.crypto.BadPaddingException e) {
             } catch (IllegalBlockSizeException e) {
             } catch (UnsupportedEncodingException e) {
@@ -40,8 +41,8 @@ import java.io.*;
         public String decrypt(String str) {
             try {
                 // Decode base64 to get bytes
-                byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
-
+                byte[] dec = java.util.Base64.getMimeDecoder().decode(str);
+                
                 // Decrypt
                 byte[] utf8 = dcipher.doFinal(dec);
 
